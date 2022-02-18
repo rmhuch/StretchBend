29463e61ea728ee8f12a8bc80c8933211557cd25
rmhuch <rmhuch@uw.edu>
2022-02-18 14:34:07 -0800

    Normal Parser complete

--- NMParser.py
+++ NMParser.py
@@ -1,25 +1,44 @@
 from McUtils.GaussianInterface import GaussianLogReader
 import numpy as np
 import os
+import csv
 
 def pull_block(logfile):
     """
     This function reads a log file and pulls out the full "Frequencies" block. (Harmonic Normal Modes)
-    :return:
-    :rtype:
+    :return: Block of lines encompassing all harmonic Normal Mode info.
+    :rtype: str
     """
     with GaussianLogReader(logfile) as reader:
         parse = reader.parse("HarmonicFrequencies")
     raw_data = parse["HarmonicFrequencies"]
     return raw_data
 
+def format_freqs(block):
+    """
+    Formats Harmonic frequencies from log file into 1D array.
+    :param block: Result of `pull_block`
+    :type block: str
+    :return: `freqs` n-modes X 1
+    :rtype: np.array
+    """
+    lines = block.splitlines(False)
+    freqs = []
+    for l in lines:
+        if "Frequencies" in l:
+            freqs.append([float(x) for x in l.split()[2:]])
+        else:
+            pass
+    freqs = np.array(freqs).flatten()
+    return freqs
+
 def format_disps(block):
     """
-
-    :param block:
-    :type block:
-    :return:
-    :rtype:
+    Formats Harmonic Normal Mode Displacements from log file into a n-modes X n-atoms X 3 (X Y Z) array
+    :param block: Result of `pull_block`
+    :type block: str
+    :return: `disps` n-modes X n-atoms X 3 (X Y Z)
+    :rtype: np.array
     """
     lines = block.splitlines(False)
     disps = []
@@ -59,12 +78,44 @@ def format_disps(block):
     disps = np.array(disps)
     return disps
 
+def calcMaxDisps(disps_array, freqs, filename):
+    """
+    Takes the stack of NM displacements, calculate the displacement magnitude then determine
+    the top three displacements for each mode.
+    :param disps_array:
+    :type disps_array:
+    :param freqs:
+    :type freqs:
+    :param filename:
+    :type filename: str
+    :return: ** saves a csv file to given `filename`
+    :rtype: csv
+    """
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        # create header for csv
+        header = [str(n) for n in np.arange(len(disps_array[0]))]
+        header.insert(0, "Frequency")
+        header.insert(0, "Mode")
+        csvwriter.writerow(header)
+
+        # write each row
+        for i, mode in enumerate(disps_array):
+            norm_disps = np.linalg.norm(mode, axis=1)
+            row = list(norm_disps)
+            row.insert(0, freqs[i])
+            row.insert(0, i)
+            csvwriter.writerow(row)
+
 
 if __name__ == '__main__':
-    print('hi RSCH')
     docs = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
     MoleculeDir = os.path.join(docs, "stretch_bend", "tetramer_16", "cage")
-    f1 = os.path.join(MoleculeDir, "w4c_Hw1.log")
+    f1 = os.path.join(MoleculeDir, "w4c_oneH8.log")
     b = pull_block(f1)
     disps = format_disps(b)
+    freqs = format_freqs(b)
+    filename = os.path.join(MoleculeDir, "H8_NMdisps.csv")
+    calcMaxDisps(disps, freqs, filename)
 
